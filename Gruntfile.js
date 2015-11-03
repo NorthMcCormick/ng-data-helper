283531730ae0d998c5368671ab824ac358d24ad5
North McCormick <northmccormick@icloud.com>
2015-11-03 09:23:40 -0800

    + First list

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,40 @@ module.exports = function(grunt) {
 				dest: 'build',
 				expand: true
 			},
+			finishBuild: {
+				cwd: 'build',
+				src: [ '**' ],
+				dest: 'dist',
+				expand: true
+			}
 		},
-
+		clean: {
+			build: {
+				src: [ 'build' ]
+			},
+		},
+		uglify: {
+			build: {
+				options: {
+					mangle: true
+				},
+				files: {
+					'build/ngdatahelper.js': ['build/**/*.js']
+				}
+			}
+		}
 	});
 
 	// load the tasks
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// define the tasks
+
+	grunt.registerTask(
+		'build',
+		'Compiles all assets and minifies to dist',
+		['clean:build', 'copy:build', 'uglify:build', 'copy:finishBuild']
+	);
 };
